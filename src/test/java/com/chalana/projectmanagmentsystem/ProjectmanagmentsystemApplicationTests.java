d024b2ecb460951145f6cca1042b1c5e67563f42
chalana kalpitha <chalanakalpitha72@gmail.com>
2024-07-12 20:22:00 +0530

    initial commit

---
+++ src/test/java/com/chalana/projectmanagmentsystem/ProjectmanagmentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chalana.projectmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagmentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
